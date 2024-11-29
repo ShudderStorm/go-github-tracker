11d53c741d4b7cc647744459ca468d9e44fb4ceb
Kirill Kostogryzov <shudderstorm@yandex.ru>
2024-11-29 11:38:23 +0500

    First working version of oauth package

--- pkg/oauth/device.go
+++ pkg/oauth/device.go
@@ -22,6 +22,7 @@ type deviceAccessResponse struct {
 	АccessToken string `json:"access_token"`
 	TokenType   string `json:"token_type"`
 	Scope       string `json:"scope"`
+	Error       string `json:"error"`
 }
 
 type UserCodeRetriever func(string)
@@ -49,7 +50,16 @@ func (handler *Handler) DeviceFlow(ctx context.Context, retriever UserCodeRetrie
 		case <-ctx.Done():
 			return ctx.Err()
 		case <-ticker.C:
+			auth, err := handler.requestDeviceAccess(ctx)
+			if err != nil {
+				return fmt.Errorf("oauth device access error: %w", err)
+			}
 
+			if auth.Error == "" {
+				return nil
+			} else if auth.Error != "authorization_pending" {
+				return fmt.Errorf("oauth device access returns error code: %v", auth.Error)
+			}
 		}
 	}
 }
@@ -57,7 +67,7 @@ func (handler *Handler) DeviceFlow(ctx context.Context, retriever UserCodeRetrie
 func (handler *Handler) requestDeviceAuth(ctx context.Context) (*deviceAuthResponse, error) {
 	body := url.Values{
 		"client_id": {handler.config.clientID},
-		"scope":     {strings.Join(handler.config.scopes, "")},
+		"scope":     handler.config.scopes,
 	}.Encode()
 
 	url := handler.config.deviceAuthURL
@@ -91,6 +101,40 @@ func (handler *Handler) requestDeviceAuth(ctx context.Context) (*deviceAuthRespo
 	return auth, nil
 }
 
-func (handler *Handler) requestDeviceAccess(ctx context.Context) (deviceAccessResponse, error) {
-	return deviceAccessResponse{}, nil
+func (handler *Handler) requestDeviceAccess(ctx context.Context) (*deviceAccessResponse, error) {
+	body := url.Values{
+		"client_id":   {handler.config.clientID},
+		"device_code": {handler.config.deviceCode},
+		"grant_type":  {"urn:ietf:params:oauth:grant-type:device_code"},
+	}.Encode()
+
+	url := handler.config.deviceAccessURL
+	if url == "" {
+		return nil, fmt.Errorf("missing device access URL")
+	}
+
+	req, err := http.NewRequestWithContext(
+		ctx, http.MethodPost, url, strings.NewReader(body),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create device access request: %w", err)
+	}
+
+	req.Header = http.Header{
+		"Content-Type": {"application/x-www-form-urlencoded"},
+		"Accept":       {"application/json"},
+	}
+
+	resp, err := handler.doer.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("cannot do device access request: %w", err)
+	}
+
+	access := &deviceAccessResponse{}
+	err = json.NewDecoder(resp.Body).Decode(access)
+	if err != nil {
+		return nil, fmt.Errorf("cannot decode device access response: %w", err)
+	}
+
+	return access, nil
 }
