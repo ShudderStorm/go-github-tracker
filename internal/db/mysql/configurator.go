27966d0ece4b2e5ffd166431405ddb0b34c3aee0
Kirill Kostogryzov <shudderstorm@yandex.ru>
2025-02-20 11:23:48 +0500

    Add configurator

--- internal/db/mysql/configurator.go
+++ internal/db/mysql/configurator.go
@@ -2,26 +2,48 @@ package mysql
 
 import "github.com/go-sql-driver/mysql"
 
-type Configurator struct {
+type IConfigurator interface {
+	FormatDSN() string
+	SetUser(string) IUserConfigurator
+	SetAddres(string) IConfigurator
+	SetDBName(string) IConfigurator
+}
+
+type IUserConfigurator interface {
+	IConfigurator
+	SetPassword(string) IConfigurator
+}
+
+type configurator struct {
 	config *mysql.Config
 }
 
-func New() *Configurator {
-	return &Configurator{
+func New() *configurator {
+	return &configurator{
 		config: mysql.NewConfig(),
 	}
 }
 
-func (c *Configurator) FormatDSN() string {
+func (c *configurator) FormatDSN() string {
 	return c.config.FormatDSN()
 }
 
-func (c *Configurator) SetUser(user string) *Configurator {
+func (c *configurator) SetUser(user string) IUserConfigurator {
 	c.config.User = user
 	return c
 }
 
-func (c *Configurator) SetPassword(password string) *Configurator {
+func (c *configurator) SetPassword(password string) IConfigurator {
 	c.config.Passwd = password
+	return c
+}
+
+func (c *configurator) SetAddres(addres string) IConfigurator {
+	c.config.Addr = addres
+	return c
+}
+
+func (c *configurator) SetDBName(dbname string) IConfigurator {
+	c.config.DBName = dbname
 	return c
 }
