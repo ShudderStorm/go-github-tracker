801fb30255c2fc2392e75c965319c19d36c7bab7
Kirill Kostogryzov <shudderstorm@yandex.ru>
2025-02-28 22:54:36 +0500

    Add sqlite provider realisation

--- internal/db/sqlite/provider.go
+++ internal/db/sqlite/provider.go
@@ -1,1 +1,26 @@
 package sqlite
+
+import (
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type Provider struct {
+	connPath string
+}
+
+func (p *Provider) Open() gorm.Dialector {
+	return sqlite.Open(p.connPath)
+}
+
+func Memory() *Provider {
+	return &Provider{
+		connPath: ":memory:",
+	}
+}
+
+func File(path string) *Provider {
+	return &Provider{
+		connPath: path,
+	}
+}
