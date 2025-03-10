e00966b5ee45bdd96c100356a49f33f29dbea365
Kirill Kostogryzov <shudderstorm@yandex.ru>
2025-03-10 22:21:12 +0500

    Add Initializers

--- cmd/app/internal/config/config.go
+++ cmd/app/internal/config/config.go
@@ -1,18 +1,25 @@
 package config
 
 type Config struct {
-	Postgres struct {
-		Host     string `yaml:"host"`
-		Port     uint16 `yaml:"port"`
-		User     string `yaml:"user"`
-		Password string `yaml:"password"`
-		Database string `yaml:"database"`
-	} `yaml:"postgres"`
-	Redis struct {
-		Host string `yaml:"host"`
-		Port uint16 `yaml:"port"`
-	} `yaml:"redis"`
-	GitHub struct {
-		Secret string `yaml:"secret"`
-	}
+	Database Database `yaml:"database"`
+	Cache    Cache    `yaml:"cache"`
+	OAuth    OAuth    `yaml:"oauth"`
+}
+
+type Database struct {
+	Host     string `yaml:"host"`
+	Port     uint16 `yaml:"port"`
+	User     string `yaml:"user"`
+	Password string `yaml:"password"`
+	Database string `yaml:"database"`
+}
+
+type Cache struct {
+	Host string `yaml:"host"`
+	Port uint16 `yaml:"port"`
+}
+
+type OAuth struct {
+	ClientID string `yaml:"client_id"`
+	Secret   string `yaml:"secret"`
 }
